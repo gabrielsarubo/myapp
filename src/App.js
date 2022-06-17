b6e4445fcc34a7ee6769f6aabc42a9326cea4419
Gabriel <gabrielsarubo@hotmail.com>
2022-06-17 18:40:54 -0300

    refact: use react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import Routes from "./routes"
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+// Pages
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
-    <Routes />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to='/signin' replace />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
