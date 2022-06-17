51933861939d5682b0c3e43946ff8260625bec79
Gabriel <gabrielsarubo@hotmail.com>
2022-06-17 18:41:15 -0300

    create pages for sign in and sign up

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,46 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
+// Assets
+import './index.css';
+import Logo from '../../assets/app-logo.png';
+
 const SignIn = () => {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      <h1>SignIn page</h1>
+    // Container for the entire sign component
+    <div className="SignIn">
+    
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <img src={Logo} alt="" />
+        </div>
+        <h1 className="h3 mb-4">Entrar</h1>
+        <div className="mb-2">
+          <label htmlFor="email" className="form-label">Email</label>
+          <input type="email" id="email" className="form-control" onChange={e => setEmail(e.target.value)} placeholder="Seu email" required />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password" className="form-label">Senha</label>
+          <input type="password" id="password" className="form-control" onChange={e => setPassword(e.target.value)} placeholder="Sua senha" required />
+        </div>
+
+        <button type="submit" className="btn btn-primary w-100"
+          disabled={(!(email !== '' && password !== '')) || (false)}>
+          Entrar
+        </button>
+        <p className="mt-3 mb-0 text-muted text-center">
+          Não tem conta?&nbsp;
+          <Link to='/signup'>Crie uma agora</Link>
+        </p>
+      </form>
     </div>
-  )
+  );
 }
- 
-export default SignIn
+
+export default SignIn;
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './index.css';
+import Logo from '../../assets/app-logo.png';
+
+const SignUp = () => {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+
+  return (
+    // Container for the entire sign component
+    <div className="SignUp">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <img src={Logo} alt="" />
+        </div>
+        <h1 className="h3 mb-4">Criar conta</h1>
+        <div className="mb-2">
+          <label htmlFor="name" className="form-label">Nome</label>
+          <input type="name" id="name" className="form-control" onChange={e => { setName(e.target.value) }} placeholder="Seu nome" required />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="email" className="form-label">Email</label>
+          <input type="email" id="email" className="form-control" onChange={e => { setEmail(e.target.value) }} placeholder="Seu email" required />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password" className="form-label">Senha</label>
+          <input type="password" id="password" className="form-control" onChange={e => { setPassword(e.target.value) }} placeholder="Sua senha" required />
+        </div>
+
+        <button type="submit" className="btn btn-primary w-100"
+          disabled={(!(name !== '' && email !== '' && password !== '')) || (false)}>
+          Criar Conta
+        </button>
+        <p className="mt-3 mb-0 text-muted text-center">
+          Já tem conta?&nbsp;
+          <Link to='/signin'>Entre agora</Link>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
