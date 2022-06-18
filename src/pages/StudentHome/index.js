c32c3d7733ba2dd5a3b6473de43fdf609b5212c5
Gabriel <gabrielsarubo@hotmail.com>
2022-06-17 21:55:12 -0300

    create page StudentHome

--- src/pages/StudentHome/index.js
+++ src/pages/StudentHome/index.js
@@ -1,9 +1,50 @@
+import Header from "../../components/Header"
+import QuestionCardListItem from "../../components/QuestionCardListItem"
+
+import './index.css'
+
 const StudentHome = () => {
+  const questionCategories = [
+    { title: 'Classe Gramatical', desc: 'Uma breve descricao sobre o tipo de exercicio', iconUri: '📚' },
+    { title: 'Tradução EN → PT', desc: 'Uma breve descricao sobre o tipo de exercicio', iconUri: '🌎' },
+    { title: 'Tradução PT → EN', desc: 'Uma breve descricao sobre o tipo de exercicio', iconUri: '📝' },
+  ]
+
   return (
-    <div>
-      <h1>Student page</h1>
-    </div>
+    <>
+      <Header />
+      <div className="StudentHome">
+        <div className="container mt-5">
+          <header className="mb-4">
+            <h3 className="title">Jane Doe</h3>
+            <div className="badge-container">
+              <div className="body">⚡ 1200 Pts</div>
+            </div>
+          </header>
+          <main>
+            <div className="categories-list">
+              {
+                questionCategories.length > 0
+                  ? (
+                    questionCategories.map((category, index) => {
+                      return (
+                        <QuestionCardListItem
+                          iconUri={category.iconUri}
+                          title={category.title}
+                          desc={category.desc}
+                          key={index}
+                        />
+                      )
+                    })
+                  )
+                  : (<p>Não há categorias ainda.</p>)
+              }
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
   )
 }
- 
+
 export default StudentHome
