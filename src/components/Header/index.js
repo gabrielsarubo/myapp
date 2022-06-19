c58d4958088879354865a580a39915b84bbacccd
Gabriel <gabrielsarubo@hotmail.com>
2022-06-19 15:29:00 -0300

    update Header styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 
 import './index.css'
+import Logo from '../../assets/app-logo.png'
 
 const Header = () => {
   return (
@@ -8,12 +9,13 @@ const Header = () => {
       <div className="container">
         <nav>
           <ul>
-            <li><Link to='/student' className='active'>Inicio</Link></li>
-            <li><Link to='/student/history' className=''>Histórico</Link></li>
-            <li><Link to='/student/profile' className=''>Perfil</Link></li>
+            <li><Link to='/student' className='active'><i className="bi-bi bi-house-door"></i>&nbsp;&nbsp;Inicio</Link></li>
+            <li><Link to='/student/history' className=''><i className="bi bi-file-earmark-ruled"></i>&nbsp;&nbsp;Histórico</Link></li>
+            <li><Link to='/student/profile' className=''><i className="bi bi-person-badge"></i>&nbsp;&nbsp;Perfil</Link></li>
           </ul>
-          <div className="logo">
-            <span>MYAPP</span>
+          <div className="logo__container">
+            <img src={Logo} alt="" />
+            <span className='logo__title'>Academy</span>
           </div>
         </nav>
       </div>
