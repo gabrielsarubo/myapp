206e762bc88d8d0ba5e39cd0aee836e199ed4e6d
Gabriel <gabrielsarubo@hotmail.com>
2022-06-20 04:46:56 -0300

    create component ActionsContainer

---
+++ src/components/ActionsContainer/index.js
@@ -0,0 +1,44 @@
+import './index.css'
+
+const ActionsContainer = ({
+  questionStatus,
+  correctAsnwer,
+  handleVerify,
+  handleContinue,
+  handleFinish
+}) => {
+  return (
+    <div className="actions-container" data-state={questionStatus}>
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+          <div className="col-md-9 col-lg-7">
+            <div className="content">
+              <div className="content__left">
+                <button className="btn btn-outline-secondary btn-lg" onClick={handleFinish}>Finalizar</button>
+                {
+                  (questionStatus !== 'unanswered') && (
+                    <div className="input-group">
+                      <span className="input-group-text" id="inputGroup-sizing-default">Resposta</span>
+                      <input type="text" className="form-control" aria-describedby="inputGroup-sizing-default" defaultValue={correctAsnwer} readOnly />
+                    </div>
+                  )
+                }
+              </div>
+              <div className="content__right">
+                <button className={`btn btn-${questionStatus === 'unanswered' ? 'primary' : questionStatus === 'correct' ? 'success' : 'danger'} btn-lg`} onClick={questionStatus === 'unanswered' ? handleVerify : handleContinue}>
+                  {
+                    questionStatus === 'unanswered'
+                      ? ('Verificar')
+                      : ('Continuar')
+                  }
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ActionsContainer
