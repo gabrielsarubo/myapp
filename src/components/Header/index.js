d0cdfe052f67ac4866f3c4a3a3a2a48251f3e6cd
Gabriel <gabrielsarubo@hotmail.com>
2022-06-20 04:48:43 -0300

    update Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,27 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 import './index.css'
 import Logo from '../../assets/app-logo.png'
 
 const Header = () => {
+  const location = useLocation()
+  
   return (
     <div className="Header container-fluid">
       <div className="container">
         <nav>
           <ul>
-            <li><Link to='/student' className='active'><i className="bi-bi bi-house-door"></i>&nbsp;&nbsp;Inicio</Link></li>
-            <li><Link to='/student/history' className=''><i className="bi bi-file-earmark-ruled"></i>&nbsp;&nbsp;Histórico</Link></li>
-            <li><Link to='/student/profile' className=''><i className="bi bi-person-badge"></i>&nbsp;&nbsp;Perfil</Link></li>
+            <li><Link to='/student' className={location.pathname === '/student' ? 'active' : ''}><i className="bi-bi bi-house-door"></i>&nbsp;&nbsp;Inicio</Link></li>
+            <li><Link to='/student/history' className={location.pathname === '/student/history' ? 'active' : ''}><i className="bi bi-file-earmark-ruled"></i>&nbsp;&nbsp;Histórico</Link></li>
+            <li><Link to='/student/profile' className={location.pathname === '/student/profile' ? 'active' : ''}><i className="bi bi-person-badge"></i>&nbsp;&nbsp;Perfil</Link></li>
           </ul>
           <div className="logo__container">
             <img src={Logo} alt="" />
             <span className='logo__title'>Academy</span>
           </div>
+          <ul>
+            <li><Link to='/signin'><i className="bi bi-box-arrow-right"></i>&nbsp;&nbsp;Sair</Link></li>
+          </ul>
         </nav>
       </div>
     </div>
