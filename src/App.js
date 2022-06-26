681c57c0ed357a2f3e5762da4e34efa8dbab61b4
Hoshibi <tanyshiotani@hotmail.com>
2022-06-26 18:06:31 -0300

    feat: lógica para seleção das telas autenticadas e não-autenticada
    - se o token existir: Apenas a rotas autenticadas são exibidas
    - se o token for null: Apenas a rotas não-autenticadas são exibidas (Sign In e Sign Up)

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
+import { useContext } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-// Contexts
-import AuthContextProvider from './contexts/AuthContext'
-// Auth Pages
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-// Student pages
-import StudentHome from './pages/StudentHome'
-import StudentHistory from './pages/StudentHistory'
-import StudentProfile from './pages/StudentProfile'
-import Practice from './pages/Practice'
-// Other pages
-import NotFound from './pages/NotFound'
+import { AuthenticatedRoutes, UnauthenticatedRoutes } from './routes'
+import { AuthContext } from './contexts/AuthContext'
 
 const App = () => {
+  const { authData } = useContext(AuthContext)
   return (
-    <AuthContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Navigate to='/signin' replace />} />
-          {/* Auth routes */}
-          <Route path='/signin' element={<SignIn />} />
-          <Route path='/signup' element={<SignUp />} />
-          {/* Student routes */}
-          <Route path='/student' element={<StudentHome />} />
-          <Route path='/student/history' element={<StudentHistory />} />
-          <Route path='/student/profile' element={<StudentProfile />} />
-          {/* Practice */}
-          <Route path='/practice/:categoryId' element={<Practice />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthContextProvider>
+    <BrowserRouter>
+      {authData.userToken ? (
+        <AuthenticatedRoutes />
+      ) : (
+        <UnauthenticatedRoutes />
+      )}
+    </BrowserRouter>
   )
 }
 
